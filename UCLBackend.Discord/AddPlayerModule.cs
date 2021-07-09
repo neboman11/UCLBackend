2ec8cb53e309016fbb623b95f083bc4207626068
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-07-08 20:32:06 -0500

    Added try/catch for calls

--- UCLBackend.Discord/AddPlayerModule.cs
+++ UCLBackend.Discord/AddPlayerModule.cs
@@ -1,16 +1,20 @@
+using System;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Microsoft.Extensions.Logging;
 using UCLBackend.Discord.Interfaces.Services;
 
 namespace UCLBackend.Discord
 {
     public class AddPlayerModule : ModuleBase<SocketCommandContext>
     {
+        private readonly ILogger<AddPlayerModule> _logger;
         private readonly IUCLBackendService _uclBackendService;
 
-        public AddPlayerModule(IUCLBackendService uclBackendService)
+        public AddPlayerModule(ILogger<AddPlayerModule> logger, IUCLBackendService uclBackendService)
         {
+            _logger = logger;
             _uclBackendService = uclBackendService;
         }
 
@@ -18,22 +22,36 @@ namespace UCLBackend.Discord
         [Summary("Adds a player to the database")]
         public async Task AddPlayer(IUser user, string desiredName, string region, string rlTrackerLink, [Remainder] string altRLTrackerLinks)
         {
-            if (altRLTrackerLinks == null)
+            try
             {
-                altRLTrackerLinks = "";
+                if (altRLTrackerLinks == null)
+                {
+                    altRLTrackerLinks = "";
+                }
+                await _uclBackendService.AddPlayer(user.Id, desiredName, region, rlTrackerLink, altRLTrackerLinks.Split(' '));
+                await Context.Channel.SendMessageAsync($"{desiredName} ({user.Username}) has been added to the database.");
             }
-            await _uclBackendService.AddPlayer(user.Id, desiredName, region, rlTrackerLink, altRLTrackerLinks.Split(' '));
-            await Context.Channel.SendMessageAsync($"{user.Id} {desiredName} {region} {rlTrackerLink} {string.Join(", ", altRLTrackerLinks.Split(" "))} has been added to the database.");
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, e, $"An error occurred while adding player {user.Username}/{desiredName}.");
+                await Context.Channel.SendMessageAsync("Something went wrong, please try again.");
+            }
         }
 
         [Command("addplayer")]
         [Summary("Adds a player to the database")]
         public async Task AddPlayer(IUser user, string desiredName, string region, string rlTrackerLink)
         {
-            string altRLTrackerLinks = "";
-            
-            await _uclBackendService.AddPlayer(user.Id, desiredName, region, rlTrackerLink, altRLTrackerLinks.Split(' '));
-            await Context.Channel.SendMessageAsync($"{user.Id} {desiredName} {region} {rlTrackerLink} {string.Join(", ", altRLTrackerLinks.Split(" "))} has been added to the database.");
+            try
+            {
+                await _uclBackendService.AddPlayer(user.Id, desiredName, region, rlTrackerLink, null);
+                await Context.Channel.SendMessageAsync($"{user.Id} {desiredName} {region} {rlTrackerLink} has been added to the database.");
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, e, $"An error occurred while adding player {user.Username}/{desiredName}.");
+                await Context.Channel.SendMessageAsync("Something went wrong, please try again.");
+            }
         }
 
         [Command("dj")]
