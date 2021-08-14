b53104e2965d56c043ffad69e805df5428c3bf09
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-08-14 17:05:56 -0500

    Change deployment ports

--- UCLFrontend/Services/PlayerService.cs
+++ UCLFrontend/Services/PlayerService.cs
@@ -12,17 +12,17 @@ namespace UCLFrontend.Services
     {
         public async Task<List<Player>> GetPlayersAsync()
         {
-            return (await SendWebRequest.GetAsync<List<Player>>(new Uri("http://localhost:5100/Player/GetPlayers"), "")).OrderBy(x => x.Name).ToList();
+            return (await SendWebRequest.GetAsync<List<Player>>(new Uri("http://localhost:5000/Player/GetPlayers"), "")).OrderBy(x => x.Name).ToList();
         }
 
         public async Task<List<Player>> GetPlayersByLeagueAsync(PlayerLeague league)
         {
-            return (await SendWebRequest.GetAsync<List<Player>>(new Uri($"http://localhost:5100/Player/GetPlayersByLeague?league={league}"), "")).OrderBy(x => x.Name).ToList();
+            return (await SendWebRequest.GetAsync<List<Player>>(new Uri($"http://localhost:5000/Player/GetPlayersByLeague?league={league}"), "")).OrderBy(x => x.Name).ToList();
         }
 
         public async Task<Dictionary<PlayerLeague, double>> GetLeagueMinSalariesAsync()
         {
-            return (await SendWebRequest.GetAsync<Dictionary<PlayerLeague, double>>(new Uri("http://localhost:5100/Setting/MinSalaries"), ""));
+            return (await SendWebRequest.GetAsync<Dictionary<PlayerLeague, double>>(new Uri("http://localhost:5000/Setting/MinSalaries"), ""));
         }
     }
 }
